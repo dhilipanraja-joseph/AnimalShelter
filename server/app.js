2991f6be4c275390fe1ee18e65d7fa30c5225bca
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-08-25 11:04:11 -0700

    prod ready

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV == 'production'){
 }else{
   //WEBPACK CONFIG
   const webpack = require('webpack');
-  const webpackConfig = require('../webpack.config');
+  const webpackConfig = require('../webpack.dev');
   const compiler = webpack(webpackConfig);
 
   app.use(require('webpack-dev-middleware')(compiler, {
